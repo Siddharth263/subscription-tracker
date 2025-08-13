d73f608c40e4e054bf56b7b6c49c3d1a8886bc5e
Siddharth263 <priyanshulal2213@gmail.com>
2025-08-13 20:10:38 +0530

    authentication and authorisation completed working on arcjet and rate limiter

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ const authorize = async (req, res, next) => {
     try {
         let token;
 
-        if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
             token = req.headers.authorization.split(' ')[1];
         }
 
@@ -19,7 +19,7 @@ const authorize = async (req, res, next) => {
         if(!user) return res.status(401).json({message: 'Unauthorized'});
 
         req.user = user;
-        next(user);
+        next();
 
     } catch (error) {
         res.status(401).json({
