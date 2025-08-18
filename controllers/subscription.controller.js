1abf1cefe9a1407e713b65289f891ef18cd4e48e
Siddharth263 <priyanshulal2213@gmail.com>
2025-08-18 14:28:41 +0530

    subscription apis completed 1 left - next working on upstash, NodeMailer, and VPS

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -59,14 +59,108 @@ export const getSubscription = async (req, res, next) =>{
             message: "invalid subscription id"
         })
 
-        // checking whether the logged in user is the owner of the subscription or not
+        // checking whether the logged-in user is the owner of the subscription or not
         if(!(subscription.user.toString() === userId.toString())) return res.status(403).json({
             success: false,
-            message: "forbidden, you do not have asccess to this subscription",
+            message: "forbidden, you do not have access to this subscription",
         })
 
         return res.status(200).json({success: true, data: subscription});
     } catch (error) {
+        next(error);
+    }
+}
+
+export const updateSubscription = async (req, res, next) =>{
+    try {
+        const subscription = await Subscription.findById(req.params.id);
+
+        if(!subscription) return res.status(404).json({
+            success: false,
+            message: "invalid subscription id"
+        });
+
+        if(subscription.user.toString() !== req.user._id.toString()) return res.status(403).json({
+            success: false,
+            message: "forbidden,  you do not have the access to this subscription",
+        })
+
+        const updated = await Subscription.findOneAndUpdate(
+            {_id:req.params.id, user: req.user._id},
+            {$set: req.body},
+            {new:true, runValidators:true}
+        )
+
+        res.status(200).json({
+            success: true,
+            message: "subscription details has been updated",
+            data: updated
+        });
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const deleteSubscription = async (req, res, next) =>{
+    try {
+        const subscription = await Subscription.findById(req.params.id);
+
+        if(!subscription) return res.status(404).json({
+            success: false,
+            message: "invalid subscription id"
+        });
+
+        if(subscription.user.toString() !== req.user._id.toString()) return res.status(403).json({
+            success: false,
+            message: "forbidden,  you do not have the access to this subscription",
+        })
+
+        const deleted = await Subscription.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({
+            success: true,
+            message: "subscription has been deleted",
+            data: deleted
+        })
+
+    } catch(error) {
+        next(error);
+    }
+}
+
+export const cancelSubscription = async (req, res, next) =>{
+    try {
+        const subscription = await Subscription.findById(req.params.id);
+
+        if(!subscription) return res.status(404).json({
+            success: false,
+            message: "invalid subscription id"
+        });
+
+        if(subscription.user.toString() !== req.user._id.toString()) return res.status(403).json({
+            success: false,
+            message: "forbidden,  you do not have the access to this subscription",
+        })
+
+        if(subscription.status !== 'active') return res.status(409).json({
+            success: false,
+            message: "subscription is already either cancelled or expired",
+        })
+
+        const cancelled = await Subscription.findOneAndUpdate(
+            {_id:req.params.id, user: req.user._id},
+            {status: "cancelled"},
+            {new: true, runValidators: true}
+        )
+
+        res.status(200).json({
+            success: true,
+            message: "subscription has been cancelled",
+            data: cancelled,
+        })
+
+    } catch(error) {
         next(error);
     }
 }
--- routes/subscriptionRouter.js
+++ routes/subscriptionRouter.js
@@ -1,6 +1,13 @@
 import { Router } from 'express';
 import authorize from "../middlewares/auth.middleware.js";
-import {createSubscription, getSubscription, getUserSubscription} from "../controllers/subscription.controller.js";
+import {
+    createSubscription,
+    getSubscription,
+    getUserSubscription,
+    updateSubscription,
+    deleteSubscription,
+    cancelSubscription
+} from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 subscriptionRouter.use(authorize);
@@ -9,24 +16,16 @@ subscriptionRouter.get('/:id', getSubscription);
 
 subscriptionRouter.post('/', createSubscription);
 
-subscriptionRouter.put('/:id', (req, res) => {
-    res.send({title: "update subscriptions as per mentioned id"})
-});
+subscriptionRouter.put('/:id', updateSubscription);
 
-subscriptionRouter.delete('/:id', (req, res) => {
-    res.send({title: "delete a subscription as per mentioned id"})
-});
+subscriptionRouter.delete('/:id', deleteSubscription);
 
 subscriptionRouter.get('/user/:id', getUserSubscription);
 
-subscriptionRouter.put('/:id/cancel', (req, res) => {
-    res.send({title: "cancel a specific subscription as per mentioned id"})
-});
+subscriptionRouter.put('/:id/cancel', cancelSubscription);
 
 subscriptionRouter.get('/upcoming-renewals', (req, res) => {
     res.send({title: "get upcoming renewals subscriptions"})
 });
-
-
 
 export default subscriptionRouter;
